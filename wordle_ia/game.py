87d9422cc2501475de7e7fa53f35a842eda13c10
azevedo <azevedoantoniocicero@gmail.com>
2024-01-04 08:49:27 -0300

    fiz a tabela incial

--- wordle_ia/game.py
+++ wordle_ia/game.py
@@ -1,5 +1,19 @@
-from palavras import PALAVRAS
 from random import choice
-import rich
+
+from palavras import PALAVRAS
+from tabulate import tabulate
+from rich.console import Console
 
 
+def preprocess():
+    return
+
+
+def tabela():
+    console = Console()
+    console.print("[bold]Wordle[/bold]", justify='center')
+    conteudo = [['', '', '', '', ''], ['', '', '', '', ''],['', '', '', '', ''],['', '', '', '', ''],['', '', '', '', ''],]
+    tabela = tabulate(conteudo, tablefmt="simple_grid")
+    console.print(tabela, justify='center')
+
+tabela()
