1e381807f7888a0049ce4a6bee68850f88ae647e
azevedo <azevedoantoniocicero@gmail.com>
2024-01-05 13:24:51 -0300

    jogo feito agora tem que criar o cli com o typer

--- wordle_ia/game.py
+++ wordle_ia/game.py
@@ -1,14 +1,28 @@
 from random import choice
 
-from banco_de_palavras import CONJUNTO_DE_PALAVRAS, LISTA_DE_PALAVRAS
 from rich import box
 from rich.console import Console
 from rich.table import Table
+
+from wordle_ia.banco_de_palavras import CONJUNTO_DE_PALAVRAS
 
 console = Console()
 
 
 def cria_o_dict_palavra_sortida(palavra: str) -> dict[str, int]:
+    """
+    Cria um dicionario com a quantidade de cada letra presente da palavra
+    sortida
+
+    Parameters:
+        palavra: Uma string que representa a palavra sortida.
+    Returns:
+        Um dicionario onde as chaves são as letras e os itens a quantidade de
+        vezes que essa letra aparece.
+    Examples:
+        >>> cria_o_dict_palavra_sortida('palavra')
+        {'p': 1, 'a': 3, 'l': 1, 'v': 1, 'r': 1}
+    """
     dict_palavra_sortida = {}
 
     for letra in palavra:
@@ -20,31 +34,57 @@ def cria_o_dict_palavra_sortida(palavra: str) -> dict[str, int]:
     return dict_palavra_sortida
 
 
-PALAVRA_SORTIDA = choice(LISTA_DE_PALAVRAS)
-DICT_PALAVRA_SORTIDA = cria_o_dict_palavra_sortida(PALAVRA_SORTIDA)
-
-
 def palavra_valida(palavra: str) -> bool:
+    """
+    Verifica se a palavra a ser testada é valida.
+
+    Parameters:
+        palavra: Uma string que representa a palavra testada do usuário.
+    Returns:
+        Um booleano que verifica se a palavra testada é possível ou não.
+    Examples:
+        >>> palavra_valida('white')
+        True
+        >>> palavra_valida('antonio')
+        False
+    """
     if len(palavra) == 5 and palavra in CONJUNTO_DE_PALAVRAS:
         return True
     return False
 
 
-def preprocess(
-    palavra: str, palavra_sortida: str, dict_palavra_sortida: dict[str, int]
-) -> list[str]:
-    """Faz o pré-processamento da palavra"""
+def preprocess(palavra: str, palavra_sortida: str) -> tuple[list[str], bool]:
+    """
+    Faz o pré-processamento da palavra para conseguir colocar na tabela
+    Parameters:
+        palavra: a string escolhida pelo usuário
+    Returns:
+        Uma tupla com dois valores, o primeiro sendo uma lista com as letras
+        formatadas e um booleano representando se a palavra teste foi correta
+        ou não
+    Example:
+        >>> preprocess('white', 'white')
+        (['[bold green]W[/bold green]', ...], True)
+        >>> preprocess('piece', 'white')
+        (['[bold]P[/bold]', ...], False)
+    """
 
     lista_de_letras = [''] * 5
     palavra_lista = list(palavra)
+    dict_palavra_sortida = cria_o_dict_palavra_sortida(palavra_sortida)
 
     for i in range(5):
         letra = palavra[i]
         if letra in dict_palavra_sortida and letra == palavra_sortida[i]:
-            lista_de_letras[i] = '[bold green]' + letra.upper() + '[/bold green]'
+            lista_de_letras[i] = (
+                '[bold green]' + letra.upper() + '[/bold green]'
+            )
             dict_palavra_sortida[letra] -= 1
             if dict_palavra_sortida[letra] == 0:
                 del dict_palavra_sortida[letra]
+            # Veriricar se acertou a palavra
+            if len(dict_palavra_sortida) == 0:
+                return lista_de_letras, True
 
             palavra_lista[i] = '-'
 
@@ -55,20 +95,27 @@ def preprocess(
             and letra in dict_palavra_sortida
             and dict_palavra_sortida[letra] > 0
         ):
-            lista_de_letras[i] = '[bold yellow]' + letra.upper() + '[/bold yellow]'
+            lista_de_letras[i] = (
+                '[bold yellow]' + letra.upper() + '[/bold yellow]'
+            )
             dict_palavra_sortida[letra] -= 1
             palavra_lista[i] = '-'
 
     for i in range(5):
         if palavra_lista[i].isalpha():
-            lista_de_letras[i] = '[bold]' + palavra_lista[i].upper() + '[/bold]'
+            lista_de_letras[i] = (
+                '[bold]' + palavra_lista[i].upper() + '[/bold]'
+            )
 
-    return lista_de_letras
+    return lista_de_letras, False
 
 
-def init_tabela() -> list[str]:
+def init_tabela() -> None:
     """
-    Cria uma lista que vai servir de tabela.
+    Cria a tabela inicial vazia.
+    Examples:
+        >>> init_tabela()
+        Wordle ... ┘
     """
     tabela = Table(
         title='[bold]Wordle[/bold]',
@@ -79,18 +126,53 @@ def init_tabela() -> list[str]:
     )
     for i in range(6):
         tabela.add_row(' ', ' ', ' ', ' ', ' ')
+
+    console.print(tabela)
+
+    return
+
+
+def create_tabela() -> Table:
+    tabela = Table(
+        title='[bold]Wordle[/bold]',
+        show_header=False,
+        show_lines=True,
+        box=box.SQUARE,
+        title_justify='center',
+    )
+
     return tabela
 
 
-def __main__():
-    tabela = init_tabela()
-    console.print(tabela)
+def imprime_tentativa(tentativa: str, palavra_sortida: str) -> bool:
+    lista_de_letras, certo = preprocess(tentativa, palavra_sortida)
+    LISTA_DE_TENTATIVAS.append(lista_de_letras)
+    tabela = create_tabela()
 
-    for i in range(6):
+    for letras in LISTA_DE_TENTATIVAS:
+        tabela.add_row(*letras)
+
+    for _ in range(6 - len(LISTA_DE_TENTATIVAS)):
+        tabela.add_row(' ', ' ', ' ', ' ', ' ')
+
+    console.print(tabela)
+    return certo
+
+
+LISTA_DE_TENTATIVAS = []
+
+
+def __main__():
+    init_tabela()
+    palavra_sortida = choice(list(CONJUNTO_DE_PALAVRAS))
+    certo = False
+    print(palavra_sortida)
+    while not certo:
         tentativa = ''  # Gambiarra
         while palavra_valida(tentativa) is False:
             tentativa = input('Informe a palavra: ')
-            print(palavra_valida(tentativa))
+        certo = imprime_tentativa(tentativa, palavra_sortida)
+    print('Meus parabens')
 
 
 if __name__ == '__main__':
