6a653a4f2303563cdb628beea185687a8e92fb66
Antonio Cicero <azevedoantoniocicero@gmail.com>
2024-01-11 12:05:18 +0000

    mais um criterio de parada

--- wordle_ia/cli.py
+++ wordle_ia/cli.py
@@ -16,14 +16,20 @@ def init_game():
     init_table()
     palavra_sortida = choice(list(CONJUNTO_DE_PALAVRAS))
     certo = False
-    # jprint(palavra_sortida)
-    while not certo:
+    # print(palavra_sortida)
+    msg1 = ':party_popper: [bold green] PARABENS! [/bold green] :party_popper:'
+    msg2 = ':tired_face: [bold red] VOCÊ PERDEU [/bold red] :tired_face:'
+    qtd_tentativas = 0
+    while not certo or qtd_tentativas > 6:
         tentativa = ''  # Gambiarra
         while palavra_valida(tentativa) is False:
             tentativa = input('Informe a palavra: ')
 
+        qtd_tentativas += 1
         os.system('cls' if os.name == 'nt' else 'clear')
         certo = imprime_tentativa(tentativa, palavra_sortida)
-    console.print(
-        ':party_popper: [bold green] PARABENS! [/bold green] :party_popper:'
-    )
+    if certo:
+        console.print(msg1)
+    else:
+        console.print(msg2)
+        console.print('[bold] A palavra correta é [/bold]', palavra_sortida)
